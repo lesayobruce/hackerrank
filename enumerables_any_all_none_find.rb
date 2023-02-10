0867113e60eccd2886444706267feae6ddc97bc0
lesayobruce <lesayobruce419@gmail.com>
2023-02-10 08:33:34 +0300

    completed the test

---
+++ enumerables_any_all_none_find.rb
@@ -0,0 +1,17 @@
+def func_any(hash)
+    # your code here
+
+  hash.any? { |key, value| key.is_a? Integer }
+end
+
+def func_all(hash)
+  hash.all? { |key, value| value.is_a?(Integer) && value < 10 }
+end
+
+def func_none(hash)
+  hash.none? { |key, value| value.nil? }
+end
+
+def func_find(hash)
+  hash.find { |key, value| (key.is_a?(Integer) && value.is_a?(Integer) && value < 20) || (key.is_a?(String) && value.is_a?(String) && value[0] == 'a') }
+end
